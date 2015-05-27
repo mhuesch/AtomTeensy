bda55e5e9faaed2ebea9b584b75b38b72d0a2925
Michael Hueschen <michaelhueschen@gmail.com>
2015-05-26 23:41:06 -0700

    FibClock works!
    This is a gross first pass. I do a huge enumeration of all possible
    hour/minute combinations (couldn't think of a better way to do this
    in Atom). Right now I am just taking the first possibility of the
    enumerated solutions, but I would like to do a random choice like
    the KickStarter project does.
    
    This produces a 182K .ino file which is absurdly huge. Oh well,
    I get to do all the "heavy lifting" in Haskell.

--- src/FibClock.hs
+++ src/FibClock.hs
@@ -6,53 +6,52 @@ import Control.Monad
 import Data.Int
 import System.Directory
 
-bLo = 2
-bHi = 10
+import Enumerate (hourMin5Colorings)
 
-mLo = 15
-mHi = 17
+led5Pins = [2..4]
+led3Pins = [5..7]
+led2Pins = [8..10]
+led1aPins = [15..17]
+led1bPins = [21..23]
 
-tLo = 21
-tHi = 23
+pinLists = [ led5Pins, led3Pins, led2Pins, led1aPins, led1bPins ]
 
-allPins = [bLo..bHi]++[mLo..mHi]++[tLo..tHi]
+allPins :: (Num a, Enum a) => [a]
+allPins = concat pinLists
+
 numPins = fromIntegral $ length allPins
 
 blink :: Atom ()
 blink = do
-  muckIdx <- int32 "muckIdx" 0
-  dispIdx <- int32 "dispIdx" 0
-  pinIdx  <- int32 "pinIdx" 0
-  leds <- array "leds" $ replicate numPins True
-
-  -- How much we step by
-  muckInc <- int32 "muckInc" 0
+  hour <- int8 "hour" 0
+  min5 <- int8 "min5" 0
+  pinIdx  <- int8 "pinIdx" 0
+  --
+  leds <- array "leds" $ replicate (length pinLists) 0 -- :: Atom (A Int8)
 
   on <- bool "on" False
 
-  period 100 $ phase 0 $ atom "displayArray" $ do
-    forM (zip allPins [0..]) $ \(p,offset) -> atom ("pin" ++ show p) $ do
-      cond $ value dispIdx ==. Const offset
-      pinIdx <== Const p
-      on     <== leds !. (value dispIdx)
+
+  period 100 $ phase 0 $ atom "writeToPins" $ do
+    call "zero_pins"
+    forM (zip (map (fromIntegral . head) pinLists) [0..]) $ \(pIdx, offset) -> atom ("led" ++ show offset) $ do
+      let offset' :: E Int8
+          offset' = Const $ fromIntegral offset
+          lightVal = leds !. offset'
+      cond $ not_ $ Eq lightVal 0
+      pinIdx <== pIdx + (mod_ (lightVal - 1) 3)
+      on <== true
       call "avr_blink"
-      incr dispIdx
 
-    atom "loopDown" $ do
-      cond $ value dispIdx >=. Const numPins
-      dispIdx <== 0
-
-  period ph $ phase 0 $ atom "muckArray" $ do
-    atom "loopDown" $ do
-      cond $ value muckIdx >=. Const numPins
-      muckIdx <== (value muckIdx) - numPins
-    --
-    atom "muck" $ do
-      cond $ value muckIdx <. Const numPins
-      zeroArray leds numPins
-      let tmp = leds ! (value muckIdx)
-      tmp <== true
-      muckIdx <== (value muckIdx) + (value muckInc)
+  period 10000 $ phase 0 $ atom "updateLEDs" $ do
+    zeroArray leds (fromIntegral $ length pinLists)
+    forM hourMin5Colorings $ \(h,m5,css) -> atom ("hm5_" ++ show h ++ "_" ++ show m5) $ do
+      cond $ (value hour) `Eq` (Const (fromIntegral h))
+      cond $ (value min5) `Eq` (Const (fromIntegral m5))
+      let cs = head css
+      forM (zip cs [0..]) $ \(c,offset) -> do
+        (leds ! (Const offset :: E Int8)) <== Const (fromIntegral c)
+      
 
   period 100 $ phase 0 $ atom "readPin" $ do
     call "check_buttons"
@@ -60,12 +59,9 @@ blink = do
   where
     ph = 50000
 
-zeroArray :: A Bool -> Int32 -> Atom ()
+zeroArray :: A Int8 -> Int8 -> Atom ()
 zeroArray arr len = forM_ [0..len] $ \off -> do
-  (arr ! (Const off)) <== false
-
-negateV :: V Bool -> Atom ()
-negateV x = (x <== not_ (value x))
+  (arr ! (Const off)) <== 0
 
 
 
@@ -91,10 +87,10 @@ pinOutput i = "  pinMode(" ++ show i ++ ", OUTPUT);"
 prePostCode _ _ _ =
   ( unlines
     [ "#include <Bounce.h>"
-    , (varInit Int32 "upButtonPin" "12")
-    , (varInit Int32 "downButtonPin" "11")
-    , "Bounce upButton = Bounce(upButtonPin, 1);"
-    , "Bounce downButton = Bounce(downButtonPin, 1);"
+    , (varInit Int32 "minButtonPin" "12")
+    , (varInit Int32 "hourButtonPin" "11")
+    , "Bounce minButton = Bounce(minButtonPin, 1);"
+    , "Bounce hourButton = Bounce(hourButtonPin, 1);"
     , (varInit Int16 "ledPin" "13")
     , "void avr_blink(void);"
     , "void check_buttons(void);"
@@ -103,25 +99,32 @@ prePostCode _ _ _ =
     [ "void setup() {"
     , "  // initialize the digital pin as an output:"
     , "  pinMode(ledPin, OUTPUT);"
-    , "  pinMode(upButtonPin, INPUT);"
-    , "  pinMode(downButtonPin, INPUT);"
+    , "  pinMode(minButtonPin, INPUT);"
+    , "  pinMode(hourButtonPin, INPUT);"
     ]
     ++ map pinOutput allPins ++
     [ "}"
     , ""
     , "// set the LED on or off"
-    , "void avr_blink() { digitalWrite(state.FibClock.pinIdx, state.FibClock.on); }"
+    , "void avr_blink() { " ++ digitalWrite "state.FibClock.pinIdx" "state.FibClock.on" ++ " }"
     , "void check_buttons() {"
-    , "  if (upButton.update() && upButton.fallingEdge()) {"
-    , "    state.FibClock.muckInc += 1;"
+    , "  if (hourButton.update() && hourButton.fallingEdge()) {"
+    , "    state.FibClock.hour = (state.FibClock.hour+1) % 12;"
     , "  }"
-    , "  if (downButton.update() && downButton.fallingEdge()) {"
-    , "    state.FibClock.muckInc -= 1;"
+    , "  if (minButton.update() && minButton.fallingEdge()) {"
+    , "    state.FibClock.min5 = (state.FibClock.min5+1) % 12;"
     , "  }"
     , "}"
+    , "void zero_pins() {" ]
+    ++ map (\p -> "  " ++ digitalWrite (show p) "LOW") allPins ++
+    [ "}"
     , ""
     , "void loop() {"
     ,  "  " ++ atomName ++ "();"
     , "}"
     ])
   )
+
+digitalWrite :: String -> String -> String
+digitalWrite p o = "digitalWrite(" ++ p ++ ", " ++ o ++ ");"
+
